7da61b7f09097e09a448b9079b08440778d36539
Tinkerspy <tinkerspy@myown.mailcan.com>
2017-07-12 17:40:07 +0200

    Cleaned up esc code

--- src/Atm_mc_esc.cpp
+++ src/Atm_mc_esc.cpp
@@ -2,72 +2,61 @@
 
 // Uses the built-in hardware PWM on teensy
 // Uses the Servo library on other platforms
- 
- 
+  
+#ifdef TEENSY_FAST_PWM
 
-Atm_mc_esc& Atm_mc_esc::begin( int p, int frequency /* = 50 */ ) {
-  servo.attach( p );
-  range( 1000, 2000 );
-  speed( 0 );
-  return *this;          
-}
- 
-Atm_mc_esc& Atm_mc_esc::speed( int v ) {
-  int motor_cur_speed = map( constrain( v, 0, 1000 ), 0, 1000, min_range, max_range );
-  servo.writeMicroseconds( motor_cur_speed );
-  return *this;
-}
+// Servo + Fast PWM option on Teensy 3.x/LC platforms.
 
-int Atm_mc_esc::speed() {
-  return map( servo.readMicroseconds(), min_range, max_range, 0, 1000 );
-}
-
-Atm_mc_esc& Atm_mc_esc::range( int min_range, int max_range ) {
-  this->min_range = min_range;
-  this->max_range = max_range;
-  return *this;
-}
-
-/* 
- 
 Atm_mc_esc& Atm_mc_esc::begin( int p, int frequency ) {
   if ( frequency == -1 ) {
     servo.attach( p );
     servo_mode = true;
   } else {
-#ifdef TEENSY_HW_PWM
     motor_pin = p;
     pinMode( p, OUTPUT );
     analogWriteFrequency( motor_pin, frequency );
     analogWriteResolution( 16 ); // Global effect!
     pwm1000width = ( frequency / 50 ) * PWM_50HZ_1000US;
     analogWrite( motor_pin, 0 );
-#endif    
     servo_mode = false;
   }
   speed( 0 );
   return *this;          
 }
 
-
 Atm_mc_esc& Atm_mc_esc::speed( int v ) {
-  int motor_cur_speed = constrain( v, 0, 1000 );
+  motor_cur_speed = constrain( v, 0, 1000 );
   if ( servo_mode ) {
     servo.writeMicroseconds( motor_cur_speed + 1000 );
   } else {
-#ifdef TEENSY_HW_PWM
     analogWrite( motor_pin, map( motor_cur_speed, 0, 1000, pwm1000width, pwm1000width * 2  ) );
-#endif
     }
   return *this;
 }
 
 int Atm_mc_esc::speed() {
-#ifdef TEENSY_HW_PWM
   return motor_cur_speed;
-#else
-  return servo.readMicroseconds();
-#endif
 }
 
-*/
+#else
+
+// Basic version with Servo library only
+  
+Atm_mc_esc& Atm_mc_esc::begin( int p, int frequency /* = 50 */ ) {
+  servo.attach( p );
+  speed( 0 );
+  return *this;          
+}
+ 
+Atm_mc_esc& Atm_mc_esc::speed( int v ) {
+  int motor_cur_speed = map( constrain( v, 0, 1000 ), 0, 1000, 1000, 2000 );
+  servo.writeMicroseconds( motor_cur_speed );
+  return *this;
+}
+
+int Atm_mc_esc::speed() {
+  return map( servo.readMicroseconds(), 1000, 2000, 0, 1000 );
+}
+
+#endif
+
--- src/Atm_mc_esc.hpp
+++ src/Atm_mc_esc.hpp
@@ -13,7 +13,7 @@
 // Source: https://github.com/FortySevenEffects/arduino_midi_library/issues/65
 
 #if defined(__MKL26Z64__) || defined(__MK20DX256__) || defined(__MK62FX512__) || defined(__MK66FX1M0__ )
-#define TEENSY_HW_PWM
+#define TEENSY_FAST_PWM
 #endif
 
 class Atm_mc_esc {
@@ -21,32 +21,14 @@ class Atm_mc_esc {
  public:
   Atm_mc_esc& begin(  int p, int frequency = -1 ); 
   Atm_mc_esc& speed( int v );
-  Atm_mc_esc& range( int min_range, int max_range );
   int speed( );
  private:
   Servo servo;  
-  int16_t min_range, max_range;
-  
-};
-
-
-/*
-
-class Atm_mc_esc {
-
- public:
-   Atm_mc_esc& begin(  int p, int frequency = -1 ); // Stick to multiples of 50, max 400Hz (-1 use Servo library)
-  Atm_mc_esc& speed( int v );
-  int speed( );
- private:
-#ifdef TEENSY_HW_PWM
+#ifdef TEENSY_FAST_PWM
   int motor_cur_speed;
-  int motor_pin; 
+  bool servo_mode;  
+  int motor_pin;
   int pwm1000width;
-#endif  
-  Servo servo;  
-  bool servo_mode;
- 
+#endif 
 };
 
-*/
